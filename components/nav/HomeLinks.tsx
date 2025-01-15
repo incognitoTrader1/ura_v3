6a8aa99f984637ffd6678ef072e6a1b4c9778272
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-01-15 13:43:25 +0100

    Refactor HomeLinks component to enhance mobile navigation experience
    - Introduced a responsive Sheet component for mobile navigation, allowing users to access links via a menu icon.
    - Updated layout to conditionally render navigation links based on screen size, improving usability on smaller devices.
    - Enhanced styling for active links to improve visibility and user interaction.
    
    These changes collectively improve the responsiveness and user experience of the navigation elements in the application.

--- components/nav/HomeLinks.tsx
+++ components/nav/HomeLinks.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-import { HomeNavIcon } from "@/lib/data";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { HomeNavIcon } from "@/lib/data";
+import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
+
 function HomeLinks() {
   const pathName = usePathname();
   return (
-    <div className="flex items-center gap-8">
+    <div className="md:flex items-center gap-8 hidden">
       {HomeNavIcon.map((nav) => (
         <Link href={nav.href} key={nav.id}>
           <p
@@ -23,6 +33,35 @@ function HomeLinks() {
           </p>
         </Link>
       ))}
+      <div className="block md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Menu />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <div className="hidden">
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+              </div>
+              <div className="flex flex-col justify-between gap-4 h-full">
+                {HomeNavIcon.map((nav) => (
+                  <Link href={nav.href} key={nav.id}>
+                    <p
+                      className={cn(
+                        "transition duration-300 text-slate-100",
+                        pathName === nav.href &&
+                          "text-slate-900 font-bold stroke-4 border-2 border-orange-400 bg-orange-500/30 px-2 py-1 rounded-md"
+                      )}
+                    >
+                      {nav.label}
+                    </p>
+                  </Link>
+                ))}
+              </div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
     </div>
   );
 }
