1e13861d7726424a7f31e9c4076397d73af62116
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-01-15 13:37:43 +0100

    Refactor AppMobileNav layout for improved mobile navigation
    - Wrapped MobileNav icons in a flex container to enhance layout and spacing.
    - This change improves the overall responsiveness and usability of the mobile navigation component.

--- components/nav/AppMobileNav.tsx
+++ components/nav/AppMobileNav.tsx
@@ -11,16 +11,18 @@ function AppMobileNav() {
   const pathName = usePathname();
   return (
     <nav className="right-0 bottom-0 left-0 z-50 fixed flex justify-between items-center gap-5 md:hidden bg-white shadow-2xl px-5 py-3 border-t w-full text-slate-800">
-      {MobileNavIcon.map((nav) => (
-        <Link href={nav.href} key={nav.id}>
-          <nav.icon
-            className={cn(
-              "h-6 w-6 transition duration-300 text-slate-800",
-              pathName === nav.href && "text-orange-500 font-bold stroke-4"
-            )}
-          />
-        </Link>
-      ))}
+      <div className="flex items-center gap-8 mx-auto">
+        {MobileNavIcon.map((nav) => (
+          <Link href={nav.href} key={nav.id}>
+            <nav.icon
+              className={cn(
+                "h-6 w-6 transition duration-300 text-slate-800",
+                pathName === nav.href && "text-orange-500 font-bold stroke-4"
+              )}
+            />
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 }
